21f94e415919a960fe6953253a6e1310d9dbba4f
codebyjsa <jsaishere@gmail.com>
2026-04-21 09:57:22 +0530

    feat: connected fronted with deployed backend

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://english-club-imxa.vercel.app/api';
 
 export const userService = {
   getUserById: async (userId: string) => {
